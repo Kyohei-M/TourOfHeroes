348c5b02f0f6c9d88bda1c2f09ec1ab468df8a42
Kyohei-M <fandltotn@gmail.com>
2018-08-14 03:05:52 +0900

    Routing

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -9,19 +9,15 @@ import { HeroService } from "../hero.service";
 })
 export class HeroesComponent implements OnInit {
   heroes: Hero[];
+  
   constructor(private heroService: HeroService) { }
 
   getHeroes(): void {
     this.heroService.getHeroes()
         .subscribe(heroes => this.heroes = heroes);
   }
-  selectedHero : Hero;
-  onSelect(hero: Hero): void{
-    this.selectedHero = hero;
-  }
 
   ngOnInit() {
     this.getHeroes();
   }
-
 }
